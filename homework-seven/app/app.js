48a96213c07c40c51fdaec5834849a48bbff7d3d
matthewmasko <90282407+matthewmasko@users.noreply.github.com>
2021-11-06 22:18:53 -0400

    Add files via upload

---
+++ homework-seven/app/app.js
@@ -0,0 +1,663 @@
+var LISTS = [
+    {
+    name: "Places",
+    listItems: [
+        {
+            name: "Bank",
+            checked: false,
+            category: "places",
+        },
+
+        {
+            name: "Apple Store",
+            checked: false,
+            category: "places",
+        },
+
+        {
+            name: "Starbucks",
+            checked: false,
+            category: "places",
+        },
+
+        {
+            name: "Office",
+            checked: false,
+            category: "places",
+        },
+
+        {
+            name: "Church",
+            checked: false,
+            category: "places",
+        },
+
+        {
+            name: "Banana Republic",
+            checked: false,
+            category: "places",
+        },
+
+        {
+            name: "Target",
+            checked: false,
+            category: "places",
+        },
+
+        {
+            name: "Kroger",
+            checked: false,
+            category: "places",
+        },
+
+        {
+            name: "Culvers",
+            checked: false,
+            category: "places",
+        },
+
+        {
+            name: "Home",
+            checked: false,
+            category: "places",
+        },
+
+       
+    ],
+},
+{
+    name: "Apple Store",
+    listItems: [
+        {
+            name: "iPhone 13 Pro Max",
+            checked: false,
+            category: "Apple Store",
+        },
+
+        {
+            name: "iPad Pro 12.9 inch",
+            checked: false,
+            category: "Apple Store",
+        },
+        {
+            name: "Magic Keyboard 12.9",
+            checked: false,
+            category: "Apple Store",
+        },
+        {
+            name: "Smart folio for iPad Pro 12.9",
+            checked: false,
+            category: "Apple Store",
+        },
+        {
+            name: "16-inch MacBook Pro",
+            checked: false,
+            category: "Apple Store",
+        },
+        {
+            name: "Extra MacBook charger",
+            checked: false,
+            category: "Apple Store",
+        },
+        {
+            name: "AirPods 3",
+            checked: false,
+            category: "Apple Store",
+        },
+        {
+            name: "AirPods Max",
+            checked: false,
+            category: "Apple Store",
+        },
+        {
+            name: "iPhone case",
+            checked: false,
+            category: "Apple Store",
+        },
+        {
+            name: "Apple Watch Series 7",
+            checked: false,
+            category: "Apple Store",
+        },
+    ],
+},
+{
+    name: "Starbucks",
+    listItems: [
+        {
+            name: "Mocha",
+            checked: false,
+            category: "coffee",
+        },
+
+        {
+            name: "Venti latte",
+            checked: false,
+            category: "coffee",
+        },
+        {
+            name: "Flat white",
+            checked: false,
+            category: "coffee",
+        },
+        {
+            name: "Espresso",
+            checked: false,
+            category: "coffee",
+        },
+        {
+            name: "Cookie Crunch Frap",
+            checked: false,
+            category: "coffee",
+        },
+        {
+            name: "4 cake pops",
+            checked: false,
+            category: "coffee",
+        },
+        {
+            name: "Vanilla frap",
+            checked: false,
+            category: "coffee",
+        },
+        {
+            name: "Protein box",
+            checked: false,
+            category: "coffee",
+        },
+        {
+            name: "1lb coffee whole bean",
+            checked: false,
+            category: "coffee",
+        },
+        {
+            name: "Americano, venti",
+            checked: false,
+            category: "coffee",
+        },
+    ],
+},
+{
+    name: "Coding",
+    listItems: [
+        {
+            name: "Homework 7 assignment",
+            checked: false,
+            category: "school",
+        },
+
+        {
+            name: "Week 2 Assessment exam",
+            checked: false,
+            category: "school",
+        },
+        {
+            name: "Jungle Cook XD",
+            checked: false,
+            category: "school",
+        },
+        {
+            name: "Review 115",
+            checked: false,
+            category: "school",
+        },
+        {
+            name: "Review 215",
+            checked: false,
+            category: "school",
+        },
+        {
+            name: "W3 Schools JQuery review",
+            checked: false,
+            category: "school",
+        },
+        {
+            name: "W3 Schools JavaScript review",
+            checked: false,
+            category: "school",
+        },
+        {
+            name: "Start to sketch out final project",
+            checked: false,
+            category: "school",
+        },
+        {
+            name: "Work on portfolio website",
+            checked: false,
+            category: "school",
+        },
+        {
+            name: "Check grades",
+            checked: false,
+            category: "school",
+        },
+    ],
+},
+{
+    name: "Church To Do",
+    listItems: [
+        {
+            name: "Sermon intial prep",
+            checked: false,
+            category: "church",
+        },
+        {
+            name: "Sermon read",
+            checked: false,
+            category: "church",
+        },
+        {
+            name: "Sermon Greek work",
+            checked: false,
+            category: "church",
+        },
+        {
+            name: "Read commentaries",
+            checked: false,
+            category: "church",
+        },
+        {
+            name: "Write sermon",
+            checked: false,
+            category: "church",
+        },
+        {
+            name: "Final version of the liturgy",
+            checked: false,
+            category: "church",
+        },
+        {
+            name: "Set up sanctuary for worship",
+            checked: false,
+            category: "church",
+        },
+        {
+            name: "Map out Sunday routine/schedule",
+            checked: false,
+            category: "church",
+        },
+        {
+            name: "Pastoral visits",
+            checked: false,
+            category: "church",
+        },
+        {
+            name: "Prep for congregational meeting",
+            checked: false,
+            category: "church",
+        },
+    ],
+},
+{
+    name: "Home Projects",
+    listItems: [
+        {
+            name: "Cut grass",
+            checked: false,
+            category: "home projects",
+        },
+
+        {
+            name: "Boxes in garage",
+            checked: false,
+            category: "home projects",
+        },
+
+        {
+            name: "Organize garage",
+            checked: false,
+            category: "home projects",
+        },
+
+        {
+            name: "Clean guest room",
+            checked: false,
+            category: "home projects",
+        },
+
+        {
+            name: "Install floors",
+            checked: false,
+            category: "home projects",
+        },
+
+        {
+            name: "Clean carpets",
+            checked: false,
+            category: "home projects",
+        },
+
+        {
+            name: "Change litter box",
+            checked: false,
+            category: "home projects",
+        },
+
+        {
+            name: "Fold laundry",
+            checked: false,
+            category: "home projects",
+        },
+
+        {
+            name: "Make bed",
+            checked: false,
+            category: "home projects",
+        },
+
+        {
+            name: "Dishes",
+            checked: false,
+            category: "home projects",
+        },
+
+    ],
+},
+{
+    name: "Target",
+    listItems: [
+        {
+            name: "Tummy medicine",
+            checked: false,
+            category: "home goods",
+        },
+
+        {
+            name: "Furnace filter",
+            checked: false,
+            category: "home goods",
+        },
+
+        {
+            name: "Cat litter",
+            checked: false,
+            category: "home goods",
+        },
+        {
+            name: "Light bulbs",
+            checked: false,
+            category: "home goods",
+        },
+
+        {
+            name: "Mirror",
+            checked: false,
+            category: "home goods",
+        },
+
+        {
+            name: "Present for birthday",
+            checked: false,
+            category: "home goods",
+        },
+        {
+            name: "Coffee pot",
+            checked: false,
+            category: "home goods",
+        },
+
+        {
+            name: "Frying pan",
+            checked: false,
+            category: "home goods",
+        },
+
+        {
+            name: "Candy",
+            checked: false,
+            category: "home goods",
+        },
+        {
+            name: " People Magazine",
+            checked: false,
+            category: "home goods",
+        },
+
+    ],
+},
+{
+    name: "Banana Republic",
+    listItems: [
+        {
+            name: "3 t-shirts",
+            checked: false,
+            category: "clothing",
+        },
+
+        {
+            name: "Jeans",
+            checked: false,
+            category: "clothing",
+        },
+
+        {
+            name: "Chinos",
+            checked: false,
+            category: "clothing",
+        },
+        {
+            name: "Underwear",
+            checked: false,
+            category: "clothing",
+        },
+
+        {
+            name: "Socks",
+            checked: false,
+            category: "clothing",
+        },
+
+        {
+            name: "Chukka boots",
+            checked: false,
+            category: "clothing",
+        },
+        {
+            name: "Belt",
+            checked: false,
+            category: "clothing",
+        },
+
+        {
+            name: "Winter coat",
+            checked: false,
+            category: "clothing",
+        },
+
+        {
+            name: "Fleece",
+            checked: false,
+            category: "clothing",
+        },
+        {
+            name: "Gift card",
+            checked: false,
+            category: "clothing",
+        },
+    ],
+},
+{
+    name: "Kroger",
+    listItems: [
+        {
+            name: "Milk",
+            checked: false,
+            category: "grocery",
+        },
+
+        {
+            name: "Cheese",
+            checked: false,
+            category: "grocery",
+        },
+
+        {
+            name: "Oranges",
+            checked: false,
+            category: "grocery",
+        },
+
+        {
+            name: "Bananas",
+            checked: false,
+            category: "grocery",
+        },
+
+        {
+            name: "Coffee",
+            checked: false,
+            category: "grocery",
+        },
+
+        {
+            name: "Tea",
+            checked: false,
+            category: "grocery",
+        },
+
+        {
+            name: "Flour",
+            checked: false,
+            category: "grocery",
+        },
+
+        {
+            name: "Ice cream",
+            checked: false,
+            category: "grocery",
+        },
+
+        {
+            name: "Frozen pizzas",
+            checked: false,
+            category: "grocery",
+        },
+
+        {
+            name: "Beer",
+            checked: false,
+            category: "grocery",
+        },
+    ],
+},
+{
+    name: "Culvers",
+    listItems: [
+        {
+            name: "Butter burger",
+            checked: false,
+            category: "food",
+        },
+
+        {
+            name: "Double butter burger",
+            checked: false,
+            category: "food",
+        },
+
+        {
+            name: "Fish sandwich",
+            checked: false,
+            category: "food",
+        },
+        {
+            name: "Ice cream cone",
+            checked: false,
+            category: "food",
+        },
+
+        {
+            name: "Vanilla shake",
+            checked: false,
+            category: "food",
+        },
+
+        {
+            name: "Chocolate shake",
+            checked: false,
+            category: "food",
+        },
+        {
+            name: "French fries",
+            checked: false,
+            category: "food",
+        },
+
+        {
+            name: "Butterscotch sundae",
+            checked: false,
+            category: "food",
+        },
+
+        {
+            name: "Chicken tenders",
+            checked: false,
+            category: "food",
+        },
+        {
+            name: "Coke",
+            checked: false,
+            category: "food",
+        },
+    ],
+},
+];
+
+function itemChecked(element, listIndex, itemIndex) {
+
+$(element).parent().toggleClass("strike");
+let checkedValue = !LISTS[listIndex].listItems[itemIndex].checked; LISTS[listIndex].listItems[itemIndex].checked =checkedValue;
+// console.log("checked", LISTS);
+}
+
+function addItem(listIndex) {
+let newItemName = $("#addItem").val();
+
+let newItemObj = {
+    name: newItemName,
+    checked: false,
+    catetory: "",
+};
+
+LISTS[listIndex].listItems.push(newItemObj);
+loadListItems(listIndex);
+}
+
+function deleteItem(listIndex, idx){
+    LISTS[listIndex].listItems.splice(idx,1);
+    loadListItems(listIndex);
+}
+
+function loadListItems(listIndex) {
+    let listString = `<div class="backButton"><button onclick="loadLists()">Back</button><ul></div>`;
+$.each(LISTS[listIndex].listItems, 
+    
+    function(idx, listItem) {
+    listString += `<li id="${idx}" class="${listItem.checked ? "strike": ""}"><input ${listItem.checked ? (checked="checked") :""} type="checkbox" id="${idx}" name="${listItem.name}" 
+    onclick="itemChecked(this, ${listIndex}, ${idx})">
+    <span>${listItem.name}</span>
+    <span class="delete" onclick="deleteItem(${listIndex}, ${idx})">Delete</span></li>`;
+        });
+
+
+        listString += `</ul>
+        <div class="addItemInput">
+        <input id="addItem" type="text">
+        <button onclick="addItem(${listIndex})">Add Item</button></div>`;
+        $("#app").html(listString);
+}
+
+function loadLists() {
+    let listString = "<ul>";
+    $.each(LISTS, function(idx, list){
+listString += `<li id="${idx}" onclick="loadListItems(${idx})">${list.name} <span class="right"> Items: ${list.listItems.length}</span></li>`;
+    });
+    listString += "</ul>";
+    $("#app").html(listString);
+}
+
+function initListeners() {}
+
+$(document).ready(function () {
+    initListeners();
+});
